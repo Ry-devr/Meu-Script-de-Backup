fe9876c2dc1b54ba9544ca05adb7838e2980b7c0
Ry-devr <rtmz.developer@gmail.com>
2026-01-05 22:34:25 -0300

    teste para ver como fazer para encontrar o caminho da media fisica

--- backup.py
+++ backup.py
@@ -3,14 +3,18 @@ import subprocess
 
 ##### PACOTES PACMAN #####
 with open("pkglist.txt", "w") as f:
-    subprocess.run(["pacman", "-Qqm",], stdout=f)
+     subprocess.run(["pacman", "-Qqm",], stdout=f)
 
 ##### PACOTES AUR #####
 with open("aurlist.txt", "w") as f:
-    subprocess.run(["yay", "-Qm", "--aur"], stdout=f)
+     subprocess.run(["yay", "-Qm", "--aur"], stdout=f)
 
 usuario = subprocess.check_output(["whoami"], text=True).strip()
 
 compactar = subprocess.run([ "tar", "-czvf", "backup.tar.gz", f"/home/{usuario}/.config", f"/home/{usuario}/bin", f"/home/{usuario}/.local/share/fonts", f"/home/{usuario}/.local/share/icons", f"/home/{usuario}/.local/share/applications", f"/home/{usuario}/.zshrc" ,"aurlist.txt", "pkglist.txt"])
+
+##### teste para por automaticamente e escolher a media #####
+if os.path.isdir(f"/run/media/{usuario}/Ventoy"):
+    print("pacote existe")
 
 print(compactar)
