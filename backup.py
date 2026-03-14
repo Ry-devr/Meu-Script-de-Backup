8ce080d71078803bd57d4a4a7a38ce6d3c8d03c5
Ry-devr <rtmz.developer@gmail.com>
2026-03-14 15:23:27 -0300

    removido comando de data, e corrigido erros causado por mim mesmo :/

--- backup.py
+++ backup.py
@@ -13,15 +13,11 @@ with open("aurlist.txt", "w") as f:
 usuario = subprocess.check_output(["whoami"], text=True).strip() # Ver usuario longado, q tbm tem o memsmo nome das pasta que sera usada ;)
 
 ##### COMPRIMIR #####
-# DATA
-from datetime import datetime
-data = datetime.now().strftime("%Y-%m-%d")
 
 subprocess.run([ 
-    "tar", "-czvf", "backup-{data}.tar.gz",
+    "tar", "-czvf", f"backup.tar.gz",
     f"/home/{usuario}/Estudos",
     f"/home/{usuario}/projetos",
-    f"/home/{usuario}/Projetos\ Git/",
     f"/home/{usuario}/Imagens",
     f"/home/{usuario}/Documentos",
     f"/home/{usuario}/.config",
@@ -34,14 +30,14 @@ subprocess.run([
 ])
 
 ##### LOCAL DE SALVAMENTO DO BACKUP #####
-media_Disk = subprocess.check_output(["ls", f"/run/media/{usuario}"], text=True).strip() # Descobrir o nome da media removivel
 
-if os.path.isdir(f"/run/media/{usuario}/{media_Disk}"): # verificar se o pendrive esta
-    if os.path.exists(f"/run/media/{usuario}/{media_Disk}/backup.tar.gz"):
-        os.remove(f"/run/media/{usuario}/{media_Disk}/backup.tar.gz")
+if os.path.isdir(f"/run/media/{usuario}"): # verificar se o pendrive esta
+    media_disk = os.listdir(f"/run/media/{usuario}")
+    if os.path.exists(f"/run/media/{usuario}/{media_disk[0]}/backup.tar.gz"):
+        os.remove(f"/run/media/{usuario}/{media_disk[0]}/backup.tar.gz")
 
-    print(f"\nenviando para {media_Disk}...")
-    shutil.move("backup.tar.gz", f"/run/media/{usuario}/{media_Disk}")
+    print(f"\nenviando para {media_disk[0]}...")
+    shutil.move("backup.tar.gz", f"/run/media/{usuario}/{media_disk[0]}")
     print("arquivo!!")
 
 else: # caso nao encontre pendrive ele salva na pasta /home/usuario/backup
